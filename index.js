e8e1d9e77b17b3ccc2fe0c6ae44a3ca4217339ff
Jack Ling <j.lingyx@gmail.com>
2018-09-27 01:23:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.getElementById("nested").querySelector('div.target')
+}
+
+function increaseRankBy(n) {
+  const ranked = document.getElementById("app").querySelectorAll("ul.ranked-list");
+  
+  for (let i = 0; i<ranked.length; i++){
+    ranked[i].innerHTML = (i*n).toString();
+  }
+}
+
+function deepestChild() {
+  return document.querySelector("div#grand-node div div div div")
+}
